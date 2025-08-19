68330b3e548cfe552f5049cad3ab701b843f55ab
Joan Rosell <joan.rosellr@gmail.com>
2025-08-19 17:57:34 +0200

    feat: add bun and biomejs

--- scripts/build.mjs
+++
@@ -1,9 +0,0 @@
-import * as esbuild from "esbuild";
-
-await esbuild.build({
-  entryPoints: ["src/**/*.ts"],
-  bundle: false,
-  minify: false,
-  format: "cjs",
-  outdir: "dist",
-});
---
+++ scripts/build.ts
@@ -0,0 +1,6 @@
+await Bun.build({
+  entrypoints: ["src/main.ts"],
+  minify: false,
+  format: "cjs",
+  outdir: "dist"
+});
